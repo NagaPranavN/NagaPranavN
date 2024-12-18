0e20518c09f7317cc871987aa807c5eb6fd6d334
Naga Pranav <nagapranav@icloud.com>
2024-12-18 15:14:56 +0530

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,12 +1,12 @@
 package src;
 public class Main {
     public static void main(String[] args) {
-        // Create instances of ClassOne and ClassTwo
+      
         SVGwriter SVGwriter = new SVGwriter();
         // BirthdateCalculator SVGwriter = new BirthdateCalculator();
 
-        // Call methods from both classes
-        classOne.displayMessage();
-        classTwo.showDetails();
+       
+        SVGwriter.();
+    
     }
 }
